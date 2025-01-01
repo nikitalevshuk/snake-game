937196ae00e8439757db9db58725dfcb9af11812
nikitalevshuk <levshukn@gmail.com>
2025-01-02 00:16:06 +0300

    made empty window

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import tkinter
 import random
+import os
+
 
 ROWS = 25
 COLS = 25
@@ -7,3 +9,19 @@ TILE_SIZE = 25
 
 WINDOW_WIDTH = TILE_SIZE * COLS
 WINDOW_HEIGHT = TILE_SIZE * ROWS
+
+window = tkinter.Tk()
+window.title('Snake')
+window.resizable(width=False, height=False)
+
+canvas = tkinter.Canvas(
+    window, bg = 'black',
+    width = WINDOW_WIDTH,
+    height = WINDOW_HEIGHT,
+    borderwidth = 0,
+    highlightthickness=0
+)
+canvas.pack()
+window.update()
+
+window.mainloop()
