666d008f68eecb30f9ae3a74a38bec2e5473dddc
nikitalevshuk <levshukn@gmail.com>
2025-01-02 21:49:20 +0300

    add tile class, snake, food, draw function

--- main.py
+++ main.py
@@ -6,9 +6,15 @@ import os
 ROWS = 25
 COLS = 25
 TILE_SIZE = 25
-
 WINDOW_WIDTH = TILE_SIZE * COLS
 WINDOW_HEIGHT = TILE_SIZE * ROWS
+
+
+class Tile:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 
 window = tkinter.Tk()
 window.title('Snake')
@@ -25,6 +31,7 @@ canvas = tkinter.Canvas(
 canvas.pack()
 window.update()
 
+
 window_width = window.winfo_width()
 window_height = window.winfo_height()
 screen_width = window.winfo_screenwidth()
@@ -34,6 +41,33 @@ window_x = int((screen_width/2) - (window_width/2))
 window_y = int((screen_height/2) - (window_height/2))
 
 window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
+
+
+snake = Tile(5*TILE_SIZE, 5*TILE_SIZE)
+food = Tile(10*TILE_SIZE, 10*TILE_SIZE)
+
+def draw():
+    global snake
+
+    canvas.create_rectangle(
+        snake.x,
+        snake.y,
+        snake.x + TILE_SIZE,
+        snake.y + TILE_SIZE,
+        fill='lime green'
+    )
+
+    canvas.create_rectangle(
+        food.x,
+        food.y,
+        food.x + TILE_SIZE,
+        food.y + TILE_SIZE,
+        fill = 'red'
+    )
+
+    window.after(100, draw)
+
+draw()
 
 
 window.mainloop()
